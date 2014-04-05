ed9afd11a7f6f52fc72b3dc695f1534000e77edd
Jack Christiansen <christj6@tcnj.edu>
2014-04-05 17:11:49 -0400

    Update Integ.hs

--- Integ.hs
+++ Integ.hs
@@ -6,13 +6,18 @@ module Integ where
 
 -- then type in: approx f a b (# of divisions -- the more divisions, the more accurate the approximation), which will approximate the definite integral of f(x) from a to b.
 
-approx :: (Float -> Float) -> Float -> Float -> Float -> Float
+approx :: (Double -> Double) -> Double -> Double -> Double -> Double
 approx f a b divisions = (rawSum f a b 1 divisions)*deltaX
 	where deltaX = (b - a)/(divisions) 
 
-rawSum :: (Float -> Float) -> Float -> Float -> Float -> Float -> Float
+rawSum :: (Double -> Double) -> Double -> Double -> Double -> Double -> Double
 rawSum f a b i divisions
 	| i > divisions = 0
 	| otherwise = f (a + deltaX*(i - 0.5)) + rawSum f a b (i+1) divisions
 	where deltaX = (b - a)/(divisions)
 
+
+-- derivative approximation
+derive :: (Double -> Double) -> (Double -> Double)
+derive f x = g x
+	where g x = (f(x + 0.000000001) - f(x))/0.000000001
